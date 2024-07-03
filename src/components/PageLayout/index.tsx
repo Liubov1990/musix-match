100ab5899e20071b52c73ad38d424fdab99758a4
Liuba <1990yavorska@gmail.com>
2024-07-03 16:02:02 +0200

    Added 'header' component

--- src/components/PageLayout/index.tsx
+++ src/components/PageLayout/index.tsx
@@ -1,16 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../layout/Header";
 import Footer from "../../layout/Footer";
+import Container from "@mui/material/Container";
+import { Box } from "@mui/material";
 
 function PageLayout() {
   return (
-    <>
+    <Container maxWidth="lg" sx={{ py: 2, height: "100vh" }}>
       <Header />
-      <main>
+      <Box component="main" sx={{ flexGrow: 1 }}>
         <Outlet />
-      </main>
+      </Box>
       <Footer />
-    </>
+    </Container>
   );
 }
 
--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,12 +1,86 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Box,
+  InputBase,
+  Toolbar,
+  Typography,
+  alpha,
+  styled,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("md")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "30ch",
+      "&:focus": {
+        width: "40ch",
+      },
+    },
+  },
+}));
 
 function Header(): JSX.Element {
   return (
-    <>
-      {/* <Link to="/">Home</Link> */}
-      Header
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{
+          py: { xs: 2, md: 1 },
+          px: { xs: 3, md: 5 },
+          borderRadius: "50px",
+        }}
+      >
+        <Toolbar sx={{ flexDirection: { xs: "column", md: "row" } }}>
+          <Typography
+            variant="h5"
+            component="h1"
+            sx={{ flexGrow: 1, pb: { xs: 2, md: 0 } }}
+          >
+            LyricsFinder
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search track..."
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
