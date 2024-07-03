63e02a57b9bbd3d8613a689ab841b69a8a86c907
Liuba <1990yavorska@gmail.com>
2024-07-03 20:10:30 +0200

    Added 'Search track' component

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,54 +1,5 @@
-import {
-  AppBar,
-  Box,
-  InputBase,
-  Toolbar,
-  Typography,
-  alpha,
-  styled,
-} from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("md")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  width: "100%",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    [theme.breakpoints.up("sm")]: {
-      width: "30ch",
-      "&:focus": {
-        width: "40ch",
-      },
-    },
-  },
-}));
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import SearchTrack from "../../components/SearchTrack";
 
 function Header(): JSX.Element {
   return (
@@ -69,15 +20,7 @@ function Header(): JSX.Element {
           >
             LyricsFinder
           </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search track..."
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
+          <SearchTrack />
         </Toolbar>
       </AppBar>
     </Box>
