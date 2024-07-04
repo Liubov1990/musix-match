4c5f66806ed804c90c51f2ad9a2b0ebcbed491bf
Liuba <1990yavorska@gmail.com>
2024-07-04 13:42:48 +0200

    Deleted lyrics from context

--- src/context/LyricsContext.tsx
+++ src/context/LyricsContext.tsx
@@ -3,29 +3,22 @@ import { IChildren, ILyrics, ITrack } from "../types";
 
 type TrackListType = {
   trackList: ITrack[];
-  // lyrics: ILyrics;
   setTrackList: React.Dispatch<React.SetStateAction<ITrack[]>>;
-  // setLyrics: React.Dispatch<React.SetStateAction<ILyrics>>;
 };
 
 export const LyricsContext = createContext<TrackListType>({
   trackList: [],
-  // lyrics: {} as ILyrics,
   setTrackList: () => null,
-  // setLyrics: () => null,
 });
 
 export default function ContextProvider({
   children,
 }: IChildren): React.ReactNode {
   const [trackList, setTrackList] = useState<ITrack[]>([]);
-  // const [lyrics, setLyrics] = useState<ILyrics>({} as ILyrics);
 
   const contextValue = {
     trackList,
-    // lyrics,
     setTrackList,
-    // setLyrics,
   };
 
   return (
