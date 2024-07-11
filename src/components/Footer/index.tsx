00be9f7f56e67e14c15c94a991995690a6b0f94b
Liuba <1990yavorska@gmail.com>
2024-07-11 20:52:32 +0200

    Displayed footer content when error present

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,38 +1,29 @@
 import { useContext } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Typography } from "@mui/material";
 import { AppContext } from "../../context/AppContext";
 import { StyledBox, StyledDivider } from "./styles";
 
 function Footer(): React.ReactElement {
-  const { pathname } = useLocation();
-  const params = useParams();
-  const { id } = params;
-
-  const { lyrics, isLoading, isError } = useContext(AppContext);
-
-  const isDisplayLyrics = lyrics && !isLoading && !isError;
+  const { lyrics } = useContext(AppContext);
 
   return (
     <>
-      {isDisplayLyrics && (
-        <StyledBox component="footer">
-          <StyledDivider />
-          <Typography component="p">
-            {pathname === "/" && (
-              <>
-                <Typography component="span">Povered by </Typography>
-                <a href="https://www.musixmatch.com/" target="blank">
-                  musixmatch.com
-                </a>
-              </>
-            )}
-            {pathname === `/lyrics/track/${id}` &&
-              lyrics.lyrics_copyright &&
-              `${lyrics.lyrics_copyright}`}
-          </Typography>
-        </StyledBox>
-      )}
+      <StyledBox component="footer">
+        <StyledDivider />
+        <Typography component="p">
+          {!lyrics.lyrics_copyright ? (
+            <>
+              <Typography component="span">Povered by </Typography>
+              <a href="https://www.musixmatch.com/" target="blank">
+                musixmatch.com
+              </a>
+            </>
+          ) : (
+            `${lyrics.lyrics_copyright}`
+          )}
+        </Typography>
+      </StyledBox>
     </>
   );
 }
